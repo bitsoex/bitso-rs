5905c99c3df99785568827208086f7ccfcf91f9b
Arturo Marquez Flores <arturomf94@gmail.com>
2022-05-11 14:08:28 -0500

    Add COP books
    Specifically: `EthCop`, `XrpCop` and `UsdCop`

--- src/websocket.rs
+++ src/websocket.rs
@@ -265,4 +265,10 @@ pub enum Books {
     DotUsd,
     #[strum(serialize = "doge_usd")]
     DogeUsd,
+    #[strum(serialize = "eth_cop")]
+    EthCop,
+    #[strum(serialize = "xrp_cop")]
+    XrpCop,
+    #[strum(serialize = "usd_cop")]
+    UsdCop,
 }
